2f5597c820e98c52ad8455489c7d1d4b85dcb12b
Abbas Muhammad <abbas51813@gmail.com>
2024-05-04 00:31:54 +0300

    converstation layout

--- venv/app.py
+++ venv/app.py
@@ -51,13 +51,15 @@ def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
  
-    #for i, message in enumerate(st.session_state.chat_history):
-     #   if i % 2 == 0:
-      #      st.write(user_template.replace(
-       #         "{{MSG}}", message.content), unsafe_allow_html=True)
-        #else:
-         #   st.write(bot_template.replace(
-          #      "{{MSG}}", message.content), unsafe_allow_html=True)
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+          
+                 "{{MSG}}", message.content), unsafe_allow_html=True)
+    st.session_state.chat_history = None 
 
 
 def main():
@@ -76,7 +78,7 @@ def main():
     st.header("Starbor'un Yapay Zeka botu")
 
     if st.session_state.chat_history:
-        for i, message in enumerate(reversed(st.session_state.chat_history)):
+        for i, message in enumerate(st.session_state.chat_history):
             if i % 2 == 0:
                 st.write(user_template.replace(
                     "{{MSG}}", message.content), unsafe_allow_html=True)
